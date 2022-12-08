cee45b7d028a2f8f94c32292f50e555ab7915842
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-12-08 17:00:03 -0500

    :tada: Random image

--- server.ts
+++ server.ts
@@ -1,3 +1,21 @@
-import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.167.0/http/server.ts";
 
-serve((req: Request) => new Response("Hello World"));
+const workingDir = Deno.cwd();
+
+let id = 1;
+
+const images: { id: number; path: URL }[] = [];
+
+for await (const dirEntry of Deno.readDir("./images")) {
+  images.push({
+    id: id++,
+    path: new URL(`${workingDir}/images/${dirEntry.name}`, import.meta.url),
+  });
+}
+
+serve(async () => {
+  const randomPic = images[Math.floor(Math.random() * images.length)];
+  const response = await fetch(randomPic.path);
+
+  return new Response(response.body);
+});
